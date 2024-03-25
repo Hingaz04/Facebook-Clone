093d4c19b8b20d278336b49fbf122603fb5f03c2
Hingaz <hhinga04@gmail.com>
2024-03-25 20:11:07 +0300

    Complete homepage

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -3,24 +3,16 @@ import Feed from "./Components/Feed/Feed";
 import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Widgets from "./Components/Widgets/Widgets";
-import Login from "./Components/Login/Login";
 
 function App() {
-  const user = null;
   return (
     <div className="app">
-      {!user ? (
-        <Login />
-      ) : (
-        <>
-          <Header />
-          <div className="app-body">
-            <Sidebar />
-            <Feed />
-            <Widgets />
-          </div>
-        </>
-      )}
+      <Header />
+      <div className="app-body">
+        <Sidebar />
+        <Feed />
+        <Widgets />
+      </div>
     </div>
   );
 }
--- fb-clone/src/firebase.js
+++ fb-clone/src/firebase.js
@@ -1,4 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDpxhY6tuSYe77IUh5n-vHcnUBvvKv27Lk",
   authDomain: "facebook-clone-229f9.firebaseapp.com",
@@ -8,3 +12,11 @@ const firebaseConfig = {
   appId: "1:132896219921:web:6c2a4c7d7fcf7c6ea2e2b0",
   measurementId: "G-WVGLJXCPXD",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
+export default firebase;
